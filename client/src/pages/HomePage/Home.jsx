74afbc2fa7c053c4a3d34a53d70e0622824996d3
harshith128 <harshith_fw11_128@masai.school>
2022-01-16 22:59:01 +0530

    all

--- client/src/pages/HomePage/Home.jsx
+++ client/src/pages/HomePage/Home.jsx
@@ -1,11 +1,11 @@
-import { Banner } from "../../components/Banner/Banner";
+// import { Banner } from "../../components/Banner/Banner";
 import { Navbar } from "../../components/Navbar/Navbar";
 
 export const Home = () => {
     return (
         <>
           <Navbar />
-          <Banner />
+          {/* <Banner /> */}
         </>
     )
 }
